3947401cf665783350ae9575f205b8f1bfa0c88d
Yaser <yaser.94ae@gmail.com>
2020-06-09 21:46:18 +0200

    user interface

---
+++ mcfly-api/src/user/interface/user.interface.ts
@@ -0,0 +1,8 @@
+import { Note } from '../../note/interface/note.interface';
+import { Document } from 'mongoose';
+
+export interface User extends Document {
+    readonly name: string;
+    readonly email: string;
+    readonly favouriteNotes: Array<string>;
+}

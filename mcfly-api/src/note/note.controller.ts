4b757632bd8100dfa2dde1fb1439f1454bbf5e01
Yaser <yaser.94ae@gmail.com>
2020-06-09 21:46:41 +0200

    user and note services

--- mcfly-api/src/note/note.controller.ts
+++ mcfly-api/src/note/note.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Res, HttpStatus } from '@nestjs/common';
+import { NoteService } from './note.service';
+import { NoteDTO } from './dto/note.dto';
 
 @Controller('note')
-export class NoteController {}
+export class NoteController {
+
+    constructor(
+        private noteService: NoteService
+    ) {}
+
+    @Post('/')
+    async createNote(@Body() note: NoteDTO, @Res() res) {
+        let newNote;
+        console.log(note);
+        try {
+            newNote = await this.noteService.createNote(note);
+        } catch (error) {
+            return res.status(HttpStatus.SERVICE_UNAVAILABLE).json({message: `${error}`});
+        }
+        return res.status(HttpStatus.OK).json({ newNote });
+    }
+}
--- mcfly-api/src/note/note.module.ts
+++ mcfly-api/src/note/note.module.ts
@@ -3,14 +3,20 @@ import { NoteController } from './note.controller';
 import { NoteService } from './note.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Note } from './schema/note.schema';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       {name: 'Note', schema: Note},
-    ])
+    ]),
   ],
   controllers: [NoteController],
-  providers: [NoteService]
+  providers: [
+    NoteService,
+  ],
+  exports: [
+    NoteService,
+  ]
 })
 export class NoteModule {}

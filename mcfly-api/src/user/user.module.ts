25e38e3bb44980676679d67852c6112f8901777b
Yaser <yaser.94ae@gmail.com>
2020-06-09 20:34:32 +0200

    users and notes models added

--- mcfly-api/src/user/user.module.ts
+++ mcfly-api/src/user/user.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {name: 'User', schema: User},
+    ])
+  ],
   controllers: [UserController],
   providers: [UserService]
 })

1128afc7c4a09d8d9b2fbc3c0bf2d53a938a760c
Yaser <yaser.94ae@gmail.com>
2020-06-11 18:24:20 +0200

    getUserById

--- mcfly-api/src/user/user.controller.ts
+++ mcfly-api/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Res, Body, HttpStatus, Get } from '@nestjs/common';
+import { Controller, Post, Res, Body, HttpStatus, Get, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO } from './dto/user.dto';
 
@@ -29,5 +29,17 @@ export class UserController {
             return res.status(HttpStatus.SERVICE_UNAVAILABLE).json({ message: `${error}` });
         }
         return res.status(HttpStatus.OK).json({ users });
+    }
+
+    @Get('/:id')
+    async userById(@Res() res, @Param('id') userId) {
+        let user;
+        console.log(userId);
+        try {
+            user = await this.userService.getUserById(userId);
+        } catch (error) {
+            return res.status(HttpStatus.SERVICE_UNAVAILABLE).json({ message: `${error}`});
+        }
+        return res.status(HttpStatus.OK).json({ user });
     }
 }
